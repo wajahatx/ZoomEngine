088887b7ad582e37156b07e34754b62253412f7d
Muhammad Wajahat <wajahat.sohail0@gmail.com>
2025-01-10 17:26:18 +0500

    version update

--- Sources/ZoomEngine/SUBoundedZoomView.swift
+++ Sources/ZoomEngine/SUBoundedZoomView.swift
@@ -8,43 +8,59 @@
 import Foundation
 import SwiftUI
 
+
 public struct SUBoundedZoomView<Content: View>: UIViewRepresentable {
     var minScale: CGFloat
     var maxScale: CGFloat
     var content: Content
-    
+
     public init(minScale: CGFloat = 1.0, maxScale: CGFloat = 4.0, @ViewBuilder content: () -> Content) {
         self.minScale = minScale
         self.maxScale = maxScale
         self.content = content()
     }
-    
+
     public func makeUIView(context: Context) -> ZoomEngineBoundedView {
-        let hostingController = UIHostingController(rootView: content)
         let zoomView = ZoomEngineBoundedView(frame: .zero, minimumZoom: minScale, maximumZoom: maxScale)
+
+        let hostingConfiguration = UIHostingConfiguration { content }
+        let hostingView = hostingConfiguration.makeContentView()
+
+        hostingView.translatesAutoresizingMaskIntoConstraints = false
+        zoomView.addSubview(hostingView)
         
-        hostingController.view.translatesAutoresizingMaskIntoConstraints = false
-        zoomView.addSubview(hostingController.view)
         NSLayoutConstraint.activate([
-            hostingController.view.topAnchor.constraint(equalTo: zoomView.topAnchor),
-            hostingController.view.leadingAnchor.constraint(equalTo: zoomView.leadingAnchor),
-            hostingController.view.trailingAnchor.constraint(equalTo: zoomView.trailingAnchor),
-            hostingController.view.bottomAnchor.constraint(equalTo: zoomView.bottomAnchor)
+            hostingView.topAnchor.constraint(equalTo: zoomView.topAnchor),
+            hostingView.leadingAnchor.constraint(equalTo: zoomView.leadingAnchor),
+            hostingView.trailingAnchor.constraint(equalTo: zoomView.trailingAnchor),
+            hostingView.bottomAnchor.constraint(equalTo: zoomView.bottomAnchor)
         ])
-        
+
         return zoomView
     }
-    
+
     public func updateUIView(_ uiView: ZoomEngineBoundedView, context: Context) {
         uiView.minScale = minScale
         uiView.maxScale = maxScale
-        
-        if let hostingView = uiView.subviews.first,
-           let hostingController = hostingView.next as? UIHostingController<Content> {
-            hostingController.rootView = content
+
+        if let hostingView = uiView.subviews.first {
+            hostingView.removeFromSuperview()
+            let newHostingConfiguration = UIHostingConfiguration { content }
+            let newHostingView = newHostingConfiguration.makeContentView()
+
+            newHostingView.translatesAutoresizingMaskIntoConstraints = false
+            uiView.addSubview(newHostingView)
+
+            NSLayoutConstraint.activate([
+                newHostingView.topAnchor.constraint(equalTo: uiView.topAnchor),
+                newHostingView.leadingAnchor.constraint(equalTo: uiView.leadingAnchor),
+                newHostingView.trailingAnchor.constraint(equalTo: uiView.trailingAnchor),
+                newHostingView.bottomAnchor.constraint(equalTo: uiView.bottomAnchor)
+            ])
         }
     }
 }
+
 
 public extension View {
     func zoomable(minScale: CGFloat = 1.0, maxScale: CGFloat = 4.0) -> some View {
