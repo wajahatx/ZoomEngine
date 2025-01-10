86b4181e3a720c7d82409ee8ad7182223e0c6d1d
Muhammad Wajahat <wajahat.sohail0@gmail.com>
2025-01-10 12:37:42 +0500

    Initial Commit

---
+++ Package.swift
@@ -0,0 +1,21 @@
+// swift-tools-version: 6.0
+// The swift-tools-version declares the minimum version of Swift required to build this package.
+
+import PackageDescription
+
+let package = Package(
+    name: "ZoomEngine",
+    products: [
+        // Products define the executables and libraries a package produces, making them visible to other packages.
+        .library(
+            name: "ZoomEngine",
+            targets: ["ZoomEngine"]),
+    ],
+    targets: [
+        // Targets are the basic building blocks of a package, defining a module or a test suite.
+        // Targets can depend on other targets in this package and products from dependencies.
+        .target(
+            name: "ZoomEngine"),
+
+    ]
+)
